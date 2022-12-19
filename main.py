9afb3d51ff3333d3a5cc6708aad13d696a1a8dae
Mason C <43713560+mercurrie@users.noreply.github.com>
2022-12-19 08:44:59 -0600

    Update main.py
    changed print_ui() to snake case,
    made quality of life changes to compare(),

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ from game_data import data
 # compare() calculates which choice has more followers and checks if user's answer is that choice. Returns bool
 def compare(choice_a: dict, choice_b: dict) -> bool:
     user_answer = input('Who has more instagram followers? Type "A" or "B": ').lower()
-    
-    if choice_a['follower_count'] > choice_b['follower_count']:
-        solution = 'a'
-    elif choice_b['follower_count'] > choice_a['follower_count']:
-        solution = 'b'
-    else:
-        solution = user_answer
+    solution = 'a' if choice_a['follower_count'] > choice_b['follower_count'] else 'b'
+    if choice_a['follower_count'] == choice_b['follower_count']: solution = user_answer
         
-    if user_answer == solution:
-        return True
-    else: 
-        return False
+    return user_answer == solution
 
 # coninue_game() calls compare(). While compare returns True game will continue. Returns final score
 def continue_game(subject1: dict, subject2: dict) -> int:
@@ -30,12 +22,12 @@ def continue_game(subject1: dict, subject2: dict) -> int:
         print(f"You're right! Current score: {score}.")
         subject1 = subject2
         subject2 = random.choice(data)
-        print_UI(subject1 = subject1, subject2 = subject2)
+        print_ui(subject1 = subject1, subject2 = subject2)
         end = compare(choice_a = subject1, choice_b = subject2)
     return score
 
-# print_UI() prints artwork and two random data to console
-def print_UI(subject1: dict, subject2: dict):
+# print_ui() prints artwork and two random data to console
+def print_ui(subject1: dict, subject2: dict):
     print(art.logo)
     print(f"Compare A: {subject1['name']}, a {subject1['description']}, from {subject1['country']}.")
     print(art.vs)
@@ -45,7 +37,7 @@ def play():
     subject1 = random.choice(data)
     subject2 = random.choice(data)
     
-    print_UI(subject1 = subject1, subject2 = subject2)
+    print_ui(subject1 = subject1, subject2 = subject2)
     final_score = continue_game(subject1 = subject1, subject2 = subject2)
     os.system('cls')
     print(art.logo)
